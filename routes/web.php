2c4bd2825fddfe6b16e9865e2eeae3ff62eaf10e
Eddy Loayza <eddy.loayza@outlook.com>
2024-08-22 23:45:13 -0500

    CRUD COMPLETO 2

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::view('/about', 'about')->name('about');
 //Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 //Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
+// CRUD COMPLETO 2
+
 Route::resource('blog', PostController::class, [
     'names' => 'posts',
     'parameters' => [
